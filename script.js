132f4dba220c39bcc28c397425e4c9415227ae34
zeeddyyyy <87855090+zeeddyyyy@users.noreply.github.com>
2026-02-18 17:27:51 +0100

    Create math operation functions

---
+++ script.js
@@ -0,0 +1,16 @@
+function sum(num1,num2){
+    return num1 + num2;
+}
+
+function sub(num1,num2){
+    return num1 - num2;
+}
+
+function div(num1,num2){
+    if(num1 === 0 || num2 === 0) return "Division by 0 is not allowed";
+    return num1 / num2;
+}
+
+function multi(num1,num2){
+    return num1 * num2;
+}
