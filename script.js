0797f6fa8b1e274ea1d45dcaa62298678b2522ae
zeeddyyyy <87855090+zeeddyyyy@users.noreply.github.com>
2026-02-22 15:49:41 +0100

    Update calculator display and improve styling

--- script.js
+++ script.js
@@ -8,10 +8,31 @@ function sub(num1,num2){
 }
 
 function div(num1,num2){
-    if(num1 === 0 || num2 === 0) return "Division by 0 is not allowed";
+    if(num1 === 0 || num2 === 0) return "Error";
     return num1 / num2;
 }
 
 function multi(num1,num2){
     return num1 * num2;
+}
+
+
+
+
+let num1;
+let num2;
+let operator;
+
+const operations = {
+    "+": sum,
+    "-": sub,
+    "*": multi,
+    "/": div
+};
+
+function operate(operator,num1,num2){
+    if (!operations[operator]) {
+        return "Error";
+    }
+    return operations[operator](num1,num2);
 }
